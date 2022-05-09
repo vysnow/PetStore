4f12464cdcf3bc5a00516f0f0b1ac542e8a9d6e0
betterjohn <66306036+betterjohn@users.noreply.github.com>
2022-05-09 18:21:14 +0800

    add restful routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,11 @@ $routes->get('/', 'Home::index');
 /**
  * API docs - the Swagger UI page
  */
-$routes->get('/api/v1/docs', 'Home::docs');
+
+//path: app/Config/Routes.php
+$routes->group('api', function($routes) {
+$routes->group('v1', function($routes) {
+    $routes->get('docs', 'Home::docs');
+    $routes->resource('pet');
+});
+});
